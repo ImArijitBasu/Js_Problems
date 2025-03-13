c55667c83db52c8ac10f7b180e1a205a99b8aa22
Arijit <arijitbasubd@gmail.com>
2025-03-13 19:51:12 +0600

    remove duplicates

--- countOfSubstrings.js
+++ countOfSubstrings.js
@@ -1,29 +1,39 @@
 var countOfSubstrings = function(word, k) {
-    let vowels = new Set(['a','e','i','o','u']);
-    let left = 0;
-    let count = 0;
-    let vowelSet = new Set();
+    let vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let left = 0, start = 0, count = 0;
+    let vowelMap = new Map();
     let consonantCount = 0;
 
-    for(let right = 0;right < word.length;right++){
-        if(vowels.has(word[right])){
-            vowelSet.add(word[right]);
-        }
-        else{
+    for (let right = 0; right < word.length; right++) {
+        if (vowels.has(word[right])) {
+            vowelMap.set(word[right], (vowelMap.get(word[right]) || 0) + 1);
+        } else {
             consonantCount++;
         }
-        while(consonantCount > k){
-            if(vowels.has(word[left])){
-                vowelSet.delete(word[left]);
-            }else{
+
+        while (left < right && consonantCount > k) {
+            if (vowels.has(word[left])) {
+                vowelMap.set(word[left], vowelMap.get(word[left]) - 1);
+                if (vowelMap.get(word[left]) === 0) {
+                    vowelMap.delete(word[left]);
+                }
+            } else {
                 consonantCount--;
             }
             left++;
+            start = left;
         }
-        if(vowelSet.size === 5 && consonantCount === k){
-            count++;
+
+        while (vowels.has(word[left]) && vowelMap.get(word[left]) > 1) {
+            vowelMap.set(word[left], vowelMap.get(word[left]) - 1);
+            left++;
+        }
+
+        if (vowelMap.size === 5 && consonantCount === k) {
+            count += (left - start) + 1;
         }
     }
     return count;
 };
+
 console.log(countOfSubstrings("ieaouqqieaouqq", 1));
